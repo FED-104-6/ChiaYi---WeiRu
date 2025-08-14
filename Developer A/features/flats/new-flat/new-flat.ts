4f68f20e269a90bc2cf37fb06f424eb0c5ddec58
Christineabc <smile11kb19@gmail.com>
2025-08-14 16:04:33 -0700

    Resolve merge conflicts

--- Developer A/features/flats/new-flat/new-flat.ts
+++ Developer A/features/flats/new-flat/new-flat.ts
@@ -2,10 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-flat',
-  imports: [],
-  templateUrl: './new-flat.html',
-  styleUrl: './new-flat.scss'
+  template: `
+    <div class="new-flat-form">
+      <!-- 在此處插入 HTML 內容 -->
+    </div>
+  `,
+  styles: [`
+    .new-flat-form {
+      /* 在此處插入 CSS 內容 */
+    }
+  `]
 })
-export class NewFlat {
+export class NewFlatComponent {
+  flat = { name: '', price: 0, beds: 0, description: '' };
 
+  onSubmit() {
+    // 處理新增邏輯
+    console.log('New flat added:', this.flat);
+  }
 }
