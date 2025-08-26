ef7385f0da942a3e27d4388e35a1524ef878fea9
WeiRu <daiweiru0931@gmail.com>
2025-08-26 16:56:41 -0700

    home file to main branch

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 import { HeaderComponent } from '../../core/header/header.component';
-import { AuthService, UserRole } from '../../features/auth/auth.service';
+import { AuthService } from '../../features/auth/auth.service';
+import { NewFlatComponent } from '../flats/new-flat/new-flat.component';
 
 @Component({
   selector: 'app-home',
@@ -10,29 +11,21 @@ import { AuthService, UserRole } from '../../features/auth/auth.service';
   imports: [
     CommonModule,
     RouterModule,
-    HeaderComponent
+    HeaderComponent,
+    NewFlatComponent
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   language: 'en' | 'zh' = 'en';
-  currentSlide = 5;
-  totalSlides = 5;
-  isPaused = false;
 
   constructor(
     public authService: AuthService,
     private router: Router
   ) {}
 
-  ngOnInit(): void {
-    /* 初始化時判斷角色
-    const role: UserRole = this.authService.currentUserRole();
-    if (role === 'admin') {
-      this.router.navigate(['/profile']);
-    }*/
-  }
+  ngOnInit(): void {}
 
   setLanguage(lang: 'en' | 'zh'): void {
     this.language = lang;
